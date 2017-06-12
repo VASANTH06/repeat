9ceb7ed8d12035d9b81d6e296a795c7a38acb554
VASANTH06 <vasanthrgm@gmail.com>
2017-06-11 23:33:21 -0700

    Create Repeat.java

---
+++ Repeat.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class Repeat
+{
+  public static void main(String args[])
+  { 
+    Scanner scan=new Scanner(System.in);
+    int a=scan.nextInt();
+    int i,j;
+    int s=0;
+    int b[]=new int[a];
+    for(i=0;i<a;i++)
+    {
+    b[i]=scan.nextInt();
+    }
+    for(i=0;i<a;i++)
+    {
+      for(j=i+1;j<a;j++)
+      { 
+        if(b[i]==b[j])
+        {
+        s++;
+        break;
+        }
