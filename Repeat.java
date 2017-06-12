a2ec9d9700872c5335e776423f184eb74d7ad8d9
VASANTH06 <vasanthrgm@gmail.com>
2017-06-11 23:42:07 -0700

    Create Repeat.java

--- Repeat.java
+++ Repeat.java
@@ -1,33 +1,40 @@
+ackage element;
 import java.io.*;
 import java.util.*;
-class Repeat
-{
-  public static void main(String args[])
-  { 
-    Scanner scan=new Scanner(System.in);
-    int a=scan.nextInt();
-    int i,j;
-    int s=0;
-    int b[]=new int[a];
-    for(i=0;i<a;i++)
-    {
-    b[i]=scan.nextInt();
-    }
-    for(i=0;i<a;i++)
-    {
-      for(j=i+1;j<a;j++)
-      { 
-        if(b[i]==b[j])
+public class Element {
+
+  
+    public static void main(String[] args) {
+  
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int i,j;
+        int a1=0;
+        int b[]=new int[a];
+        for(i=0;i<a;i++)
         {
-        s++;
-        break;
+            b[i]=scan.nextInt();
         }
-        if(s!=0)
-          break;
-      }
-      if(i!=a)
-      {
-        System.out.print(b[i]);
-      }
-    }
-  }
+        for(i=0;i<a;i++)
+        {
+            for(j=i+1;j<a;j++)
+            {
+                if(b[i]==b[j])
+                {
+                  a1++;
+                  break;
+                   
+                }
+            }
+            if(a1!=0)
+                break;
+        }
+       
+        if(i!=a)
+        {
+            System.out.print(b[i]);
+        }
+        }
+        }
+    
+    
